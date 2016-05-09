3bcb52f76ce32e42044e126f21f83cacf92ba181
Maegorn <mattia.pellegrino@live.it>
2016-05-09 17:11:55 +0200

    Remove deprecated flag
    EXTENDED_FONT_FOOTPRINT is set by default on M

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,9 +115,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
 # GPS
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 USE_DEVICE_SPECIFIC_GPS := true
