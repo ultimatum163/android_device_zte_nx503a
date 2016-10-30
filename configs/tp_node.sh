2fac41717693d0364107edced14dfe10b1e0e97a
Mattia Pellegrino <mattia.pellegrino@live.it>
2016-10-30 21:33:26 +0100

    init.qcom.rc: update & enable wakeup proximity control

--- configs/tp_node.sh
+++ configs/tp_node.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/system/bin/sh
 
 if [ ! -d "/data/tp" ]; then
     mkdir /data/tp
@@ -8,6 +8,28 @@ else
     rm /data/tp/*
 fi
 
+link_if_exists() {
+    if [ -e "$2" ]; then
+        chown system:system "$2"
+        ln -s "$2" "/data/tp/$1"
+    fi
+}
+
+# Keypad
+# ==============================================================================
+# Synaptics
+
+link_if_exists "keypad_enable" \
+    "/sys/bus/i2c/devices/2-0022/input/input0/0dbutton"
+
+# ==============================================================================
+# Cyttsp4
+
+link_if_exists "keypad_enable" \
+    "/sys/bus/ttsp4/drivers/cyttsp4_btn/cyttsp4_btn.main_ttsp_core/button"
+
+#Dt2w
+# ==============================================================================
 # Goodix GT915L
 
 if [ -e "/sys/bus/i2c/devices/2-005d/ic_ver" ]; then
