8c9db629889fd3a1fb65fe9289fa1d394092df5d
Maegorn <mattia.pellegrino@live.it>
2016-11-18 20:17:08 +0100

    nx503a: switch to CM POWERHAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,9 @@ TARGET_KERNEL_CONFIG := msm8974-NX503A_nomodules_defconfig
 TARGET_ZTEMT_DTS := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := NX503A,nx503a,Z5S,z5s,NX503AJ,nx503aj,Z5SN,z5sn
 
